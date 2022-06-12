5d8079d21a5d1723a6fc020461d50f47fe4fda1d
Mystel <mystelaurora@gmail.com>
2022-06-11 23:53:29 -0400

    Day 4

---
+++ practice4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+// messing with unsigneds - it worked!
+// int main()
+// {
+//     for (unsigned int i = 10; i < 11; --i)
+//     {
+//         std::cout << i << std::endl;
+//     }
+// }
+
+// exercise 2.1.2
+// int main()
+// {
+    // unsigned u = 10, u2 = 42;
+    // std::cout << u2 - u << std::endl; // 32
+    // std::cout << u - u2 << std::endl; // 4294967264
+
+    // int i = 10, i2 = 42;
+    // std::cout << i2 - i << std::endl; // 32
+    // std::cout << i - i2 << std::endl; // -32
+    // std::cout << i - u << std::endl;  // 0
+    // std::cout << u - i << std::endl;  // 0
+
+    // for (int x = 0; x < 256; ++x)
+    // {
+    //     std::cout << x << ": " << char(x) << std::endl;
+    // }
+// }
