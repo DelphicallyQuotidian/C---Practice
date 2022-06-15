0932c0416cf727958d0375f2fed71ff99edbb29f
Mystel <mystelaurora@gmail.com>
2022-06-15 17:05:41 -0400

    Day 6

---
+++ practice6.cpp
@@ -0,0 +1,27 @@
+// Exercise 2.2.1 - 
+#include <iostream>
+#include <string>
+
+// 2.2.1
+// int main()
+// {
+//     std::cin >> int input_value; // error: expected primary-expression before 'int'
+//     int i = { 3.14 };
+//     double salary = wage = 9999.99; // error: 'wage' was not declared in this scope
+//     int i = 3.14;
+// }
+
+// std::string global_str;  // blank
+// int global_int;  // 0
+
+// int main()
+// {
+//     int local_int;  // 0
+//     std::string local_str;  // blank
+
+//     std::cout << global_str << std::endl;
+//     std::cout << global_int << std::endl;
+//     std::cout << local_int << std::endl;
+//     std::cout << local_str << std::endl;
+// }
+
